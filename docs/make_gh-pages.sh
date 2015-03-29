c5ceb81b2d9781d2709115205c1f25944916243f
Justin Ellis <justin.ellis18@gmail.com>
2015-03-28 23:00:20 -0700

    Tweaked shell script a bit more

--- docs/make_gh-pages.sh
+++ docs/make_gh-pages.sh
@@ -9,14 +9,14 @@ GH_PAGES_SOURCES='docs PTMCMCSampler'
 cd ../
 git checkout gh-pages
 rm -rf build _sources _static
-git checkout master $(GH_PAGES_SOURCES)
+git checkout master -- $GH_PAGES_SOURCES
 git reset HEAD
 cd docs
 make html
 cd ../
 mv -fv docs/_build/html/* .
-rm -rf $(GH_PAGES_SOURCES)
+rm -rf $GH_PAGES_SOURCES
 git add -A
-git commit -m "Generated gh-pages for `git log master -1 --pretty=short --abbrev-commit`"
+git commit -m "Generated gh-pages for $ID"
 git push origin gh-pages
 git checkout master
