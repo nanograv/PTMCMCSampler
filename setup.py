5c311ead3b878211832e8ec88041f1df94d45f34
Justin Ellis <justin.ellis18@gmail.com>
2015-07-07 16:45:03 -0700

    Small changes to setup.py

--- setup.py
+++ setup.py
@@ -19,16 +19,15 @@ setup(
     author="Justin A. Ellis",
     author_email="justin.ellis18@gmail.com",
     packages=["PTMCMCSampler"],
-    package_dir = {'PTMCMCSampler': 'PTMCMCSampler'},
+    package_dir={'PTMCMCSampler': 'PTMCMCSampler'},
     url="https://github.com/jellis18/PTMCMCSampler",
     license="GPLv3",
-    zip_safe = False,
+    zip_safe=False,
     description="Parallel tempering MCMC sampler written in Python",
     long_description=open("README.md").read() + "\n\n"
                     + "---------\n\n"
                     + open("HISTORY.md").read(),
     package_data={"": ["README.md", "HISTORY.md"]},
-    include_package_data=True,
     install_requires=["numpy", "scipy"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
