573f47ed588e64ce446ed2c0ce7750d3fa35d811
Justin Ellis <justin.ellis18@gmail.com>
2021-03-17 17:18:58 -0400

    adding python 3.9

--- setup.py
+++ setup.py
@@ -26,5 +26,6 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
