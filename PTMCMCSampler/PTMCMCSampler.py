b5f6c9e46b4062608160d8620e99181a07b24835
Paul T. Baker <paul.baker@nanograv.org>
2021-08-18 15:50:47 -0400

    update _writeToFile docstring

--- PTMCMCSampler/PTMCMCSampler.py
+++ PTMCMCSampler/PTMCMCSampler.py
@@ -710,9 +710,9 @@ class PTSampler(object):
 
     def _writeToFile(self, iter):
         """
-        Function to write chain file. File has 3+ndim columns,
-        the first is log-posterior (unweighted), log-likelihood,
-        and acceptance probability, followed by parameter values.
+        Function to write chain file. File has ndim+4 columns,
+        appended to the parameter values are log-posterior (unnormalized),
+        log-likelihood, acceptance rate, and PT acceptance rate.
 
         @param iter: Iteration of sampler
 
