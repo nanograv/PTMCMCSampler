36581aeb66f8358a3477b47aaffaabda58ea74c5
Justin Ellis <justin.ellis18@gmail.com>
2022-11-01 06:48:00 -0400

    __version__

--- PTMCMCSampler/__init__.py
+++ PTMCMCSampler/__init__.py
@@ -1,1 +1,9 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from PTMCMCSampler import PTMCMCSampler  # noqa: F401
+
+try:
+    __version__ = version("ptmcmcsampler")
+except PackageNotFoundError:
+    # package is not installed
+    pass
