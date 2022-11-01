42cff0d77cb2df7809554f276ddbed4afd1d43a9
Justin Ellis <justin.ellis18@gmail.com>
2022-11-01 07:00:48 -0400

    go back to version file because python3.7

--- PTMCMCSampler/__init__.py
+++ PTMCMCSampler/__init__.py
@@ -1,9 +1,5 @@
-from importlib.metadata import PackageNotFoundError, version
-
 from PTMCMCSampler import PTMCMCSampler  # noqa: F401
 
-try:
-    __version__ = version("ptmcmcsampler")
-except PackageNotFoundError:
-    # package is not installed
-    pass
+from .version import version
+
+__version__ = version
